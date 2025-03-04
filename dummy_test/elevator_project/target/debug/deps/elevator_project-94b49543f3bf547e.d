747dce1dbac75b262c8fb7c4be2f5763091fa3b4
William Fosser <williafo@stud.ntnu.no>
2025-03-04 13:13:23 +0100

    Updated .gitignore to follow githubs official .gitignore for rust

--- dummy_test/elevator_project/target/debug/deps/elevator_project-94b49543f3bf547e.d
+++ dummy_test/elevator_project/target/debug/deps/elevator_project-94b49543f3bf547e.d
@@ -1,10 +1,11 @@
-/home/student/Desktop/YeahMan/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/libelevator_project-94b49543f3bf547e.rmeta: src/lib.rs src/config.rs src/master.rs src/slave.rs src/inputs.rs src/tcp.rs
+/home/student/Desktop/YeahMan/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/libelevator_project-94b49543f3bf547e.rmeta: src/lib.rs src/config.rs src/master.rs src/slave.rs src/inputs.rs src/backup.rs src/tcp.rs
 
-/home/student/Desktop/YeahMan/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/elevator_project-94b49543f3bf547e.d: src/lib.rs src/config.rs src/master.rs src/slave.rs src/inputs.rs src/tcp.rs
+/home/student/Desktop/YeahMan/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/elevator_project-94b49543f3bf547e.d: src/lib.rs src/config.rs src/master.rs src/slave.rs src/inputs.rs src/backup.rs src/tcp.rs
 
 src/lib.rs:
 src/config.rs:
 src/master.rs:
 src/slave.rs:
 src/inputs.rs:
+src/backup.rs:
 src/tcp.rs:
