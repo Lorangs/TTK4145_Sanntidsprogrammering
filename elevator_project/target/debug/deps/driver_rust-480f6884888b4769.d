b65563500e491bbfe29886d976f425210448a84a
William Fosser <fosserwilliam@gmail.com>
2025-03-06 19:26:16 +0100

    removed target forlder from git

--- elevator_project/target/debug/deps/driver_rust-480f6884888b4769.d
+++
@@ -1,7 +0,0 @@
-/home/lorang/ntnu/6_semester/TTK4145_sanntidsprogrammering/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/libdriver_rust-480f6884888b4769.rmeta: /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/lib.rs /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/elev.rs /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/poll.rs
-
-/home/lorang/ntnu/6_semester/TTK4145_sanntidsprogrammering/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/driver_rust-480f6884888b4769.d: /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/lib.rs /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/elev.rs /home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/poll.rs
-
-/home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/lib.rs:
-/home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/elev.rs:
-/home/lorang/.cargo/git/checkouts/driver-rust-0f0c9d8ec750caeb/6c287b7/src/elevio/poll.rs:
--- elevator_project/target/debug/deps/master_main-649c47f194841076.d
+++
@@ -1,5 +0,0 @@
-/home/lorang/ntnu/6_semester/TTK4145_sanntidsprogrammering/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/libmaster_main-649c47f194841076.rmeta: src/bin/master_main.rs
-
-/home/lorang/ntnu/6_semester/TTK4145_sanntidsprogrammering/TTK4145_Sanntidsprogrammering/dummy_test/elevator_project/target/debug/deps/master_main-649c47f194841076.d: src/bin/master_main.rs
-
-src/bin/master_main.rs:
