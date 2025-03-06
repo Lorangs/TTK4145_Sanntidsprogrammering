85a55d614b3cbc2b0fae616e14289f899d3219b4
Antonie24 <157692177+Antonie24@users.noreply.github.com>
2025-03-06 18:43:44 +0100

    komentar i backup_main.rs

--- elevator_project/src/bin/backup_main.rs
+++ elevator_project/src/bin/backup_main.rs
@@ -2,7 +2,8 @@ use elevator_project::{backup::Backup, config::Config, master::Master};
 use std::path::Path;
 
 
-// Main func for backup. Initializes backup. If backup crashes, it will restart and connect to a new master.
+// Main func for backup. Initializes backup. If backup crashes, it will restart and connect to a new master. 
+// We need to seperate between the backup crashing: restart as backup, and master crashing: start a master.
 fn main() {
     let config = Config::read_config(Path::new("config.json")).unwrap();
     let backup_ip = config.elevator_ip_list[1].to_string() + ":" + &config.backup_port.to_string();
