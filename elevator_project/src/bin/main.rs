c784d897bd0afaaa44b44ed935d5b9f40437bf83
Lorang Strand <lorangs@stud.ntnu.no>
2025-03-27 15:20:32 +0100

    kommentarer i main

--- elevator_project/src/bin/main.rs
+++ elevator_project/src/bin/main.rs
@@ -1,17 +1,21 @@
 use std::env;
 use std::process::Command;
 
+/// Main program. Spawn two different program threads. One master / backup, depending on input argument, and one slave unit. 
+/// Start as master + slave if 0 is passed as first argument.
+/// Start as backup + slave otherwise.
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    //Start as master
-    if args[1] == 0.to_string() {
+
+    if args[1] == 0.to_string() 
+    {
         Command::new("cargo")
             .args(["run", "--bin", "master_main"])
             .spawn()
             .expect("Failed to start master_main");
-    } else
-    //Start backup
+    } 
+    else
     {
         Command::new("cargo")
             .args(["run", "--bin", "backup_main"])
